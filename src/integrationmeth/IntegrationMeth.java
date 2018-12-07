2c7b428d84489eda723e636558a2eefc7e7f36bb
Cristian <cristianramirezdev@hotmail.com>
2018-12-06 18:27:15 -0600

    Tabulation added

--- src/integrationmeth/IntegrationMeth.java
+++ src/integrationmeth/IntegrationMeth.java
@@ -38,27 +38,154 @@ public class IntegrationMeth extends Application {
         
         boolean exit = true;
         Scanner scan = new Scanner(System.in);
+        String eq;
+        float a;
+        float b;
+        float result;
         
         while(exit) {
             System.out.println("¿Qué método quiere utilizar?\n\n1. Trapecio\n2. Simpson 1/3\n3. Simpson 3/8");
             int op = scan.nextInt();
+            scan.nextLine();
             
             switch(op) {
                 case 1:
+                    System.out.print("\n\t\t1. Por tabulación\n\t\t2. Por expresión \n\t\tOpción: ");
+                    int opti = scan.nextInt();
+                    
                     scan.nextLine();
-                    System.out.print("\n\tInserte la ecuación: ");
-                    String eq = scan.nextLine();
                     
-                    System.out.print("\ta: ");
-                    float a = scan.nextFloat();
-                    
-                    System.out.print("\tb: ");
-                    float b = scan.nextFloat();
-                    
-                    float result = Util.trapezoidalRule(a, b, eq);
-                    
-                    System.out.println("result: " + result);
+                    switch(opti) {
+                        
+                        case 1:
+                            System.out.print("Inserte a, b, f(a), f(b): ");
+                            String[] tab = scan.nextLine().split(",");
+                            
+                            float[] resultTab = new float[4];
+                            
+                            for (int i = 0; i < tab.length; i++) {
+                                resultTab[i] = Float.parseFloat(tab[i]);
+                            }
+                            
+                            float resTrap = Util.trapezoidalRule(resultTab[0], resultTab[1], resultTab[2], resultTab[3]);
+                            
+                            System.out.println("result: " + resTrap);
+                            break;
+                        
+                        case 2:
+                            System.out.print("\n\t\tInserte la ecuación: ");
+                            eq = scan.nextLine();
+
+                            System.out.print("\ta: ");
+                            a = scan.nextFloat();
+
+                            System.out.print("\tb: ");
+                            b = scan.nextFloat();
+
+                            result = Util.trapezoidalRule(a, b, eq);
+
+                            System.out.println("result: " + result);
+                            break;
+                    }
                     break;
+                    
+                case 2:
+                    System.out.print("\n\t\t1. Por tabulación\n\t\t2. Por expresión\n\t\tOpción: ");
+                    
+                    int opc = scan.nextInt();
+                    
+                    scan.nextLine();
+                    switch(opc) {
+                        case 1:
+                            System.out.print("\t\t\tInserte los valores separado por comas (x0,x1,x2,f(x0),f(x1),f(x2)): ");
+                            String valSOT = scan.nextLine();
+                            
+                            String[] values =  valSOT.split(",");
+                            
+                            float[] parsedValues = new float[6];
+                            
+                            for (int i = 0; i < values.length; i++) {
+                                parsedValues[i] = Float.parseFloat(values[i]);
+                            }
+                            
+                            float resSOT = Util.simpsonOneThree(parsedValues[0], parsedValues[1], parsedValues[2], parsedValues[3], parsedValues[4], parsedValues[5]);
+                            
+                            System.out.println("\n\t\tresult: " + resSOT);
+                            
+                            break;
+                        case 2:
+                            
+                            System.out.print("\t\t\tInserte la ecuación a evaluar: ");
+                            eq = scan.nextLine();
+                            
+                            scan.nextLine();
+                            scan.nextLine();
+                            
+                            System.out.print("\t\t\tInserte los valores 'x' separados por comas (x0,x1,x2): ");
+                            String valSOTE = scan.nextLine();
+                            
+                            String[] valuesE = valSOTE.split(",");
+                            
+                            float[] parsedValuesE = new float[3];
+                            
+                            for (int i = 0; i < valuesE.length; i++) {
+                                parsedValuesE[i] = Float.parseFloat(valuesE[i]);
+                            }
+                            
+                            result = Util.simpsonOneThree(parsedValuesE[0], parsedValuesE[1], parsedValuesE[2], eq);
+                            System.out.println("result: " + result);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                    
+                case 3:
+                    System.out.print("\n\t\t1. Por tabulación\n\t\t2. Por expresión\n\t\tOpción: ");
+                    int opt = scan.nextInt();
+                    
+                    scan.nextLine();
+                    switch(opt) {
+                        case 1:
+                            System.out.print("\t\t\tInserte los valores separado por comas (x0,x1,x2,x3,f(x0),f(x1),f(x2),f(x3)): ");
+                            String valSTE = scan.nextLine();
+                            
+                            String[] values =  valSTE.split(",");
+                            
+                            float[] parsedValues = new float[8];
+                            
+                            for (int i = 0; i < values.length; i++) {
+                                parsedValues[i] = Float.parseFloat(values[i]);
+                            }
+                            
+                            float resSTE = Util.simpsonThreeEight(parsedValues[0], parsedValues[1], parsedValues[2], parsedValues[3], parsedValues[4], parsedValues[5],parsedValues[6],parsedValues[7]);
+                            
+                            System.out.println("\n\t\tresult: " + resSTE);
+                            
+                            break;
+                        case 2:
+                            System.out.print("\t\t\tInserte la ecuación a evaluar: ");
+                            eq = scan.nextLine();
+                            
+                            System.out.print("\t\t\tInserte los valores 'x' separados por comas (x0,x1,x2,x3): ");
+                            String valSTEE = scan.nextLine();
+                            
+                            String[] valuesE = valSTEE.split(",");
+                            
+                            float[] parsedValuesE = new float[4];
+                            
+                            for (int i = 0; i < valuesE.length; i++) {
+                                parsedValuesE[i] = Float.parseFloat(valuesE[i]);
+                            }
+                            
+                            result = Util.simpsonThreeEight(parsedValuesE[0], parsedValuesE[1], parsedValuesE[2], parsedValuesE[3], eq);
+                            System.out.println("result: " + result);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                    
                 default:
                     System.out.println("Inserte una opción correcta");
                     break;
--- src/integrationmeth/util/Util.java
+++ src/integrationmeth/util/Util.java
@@ -31,17 +31,30 @@ public class Util {
     
     /**
      * 
+     * @param a float is the lower limit in the integral
+     * @param b float is the higher limit in the integral
+     * @param fa float evaluated function in a
+     * @param fb float evaluated function in b
+     * @return float The result of applying this numerical method
+     */
+    public static float trapezoidalRule(float a, float b, float fa, float fb) {
+        
+        float result = (b - a) * ((fa + fb)/2);
+        
+        return result;
+    }
+    
+    /**
+     * 
      * @param x0 float The x0 given value
      * @param x1 float The x1 given value
      * @param x2 float The x2 given value
-     * @param a float The 'a' given value
-     * @param b float The 'b' given value
      * @param eq String  The string for the equation
      * @return Simpson's 1/3 rule's result
      */
     
-    public static float simpsonOneThree(float x0, float x1, float x2, float a, float b, String eq) {
-        float h = (b - a) / 2;
+    public static float simpsonOneThree(float x0, float x1, float x2, String eq) {
+        float h = (x2 - x0) / 2;
         
         float result = (h/3) * ( substitute(eq, x0) + (4 * substitute(eq, x1)) + substitute(eq, x2) );
         
@@ -54,18 +67,52 @@ public class Util {
      * @param x1 float The x1 given value
      * @param x2 float The x2 given value
      * @param x3 float The x2 given value
-     * @param a float The 'a' given value
-     * @param b float The 'b' given value
      * @param eq String  The string for the equation
      * @return Simpson's 1/3 rule's result
      */
     
-    public static float simpsonThreeEight(float x0, float x1, float x2, float x3, float a, float b, String eq) {
-        float h = (b - a) / 3;
+    public static float simpsonThreeEight(float x0, float x1, float x2, float x3, String eq) {
+        float h = (x3 - x0) / 3;
         
-        float result = ((3*h)/8) * ( substitute(eq, x0) + (3 * substitute(eq, x1)) + (3 * substitute(eq, x2)) + substitute(eq, x3) );
+        return ((3*h)/8) * ( substitute(eq, x0) + (3 * substitute(eq, x1)) + (3 * substitute(eq, x2)) + substitute(eq, x3) );
+    }
+    
+    /**
+     * 
+     * @param x0 float The x0 given value
+     * @param x1 float The x1 given value
+     * @param x2 float The x2 given value
+     * @param fx0 float The f(x0) given value
+     * @param fx1 float The f(x1) given value
+     * @param fx2 float The f(x2) given value
+     * @return result The result of Simpson's 1/3's rule applied
+     */
+    
+    public static float simpsonOneThree(float x0, float x1, float x2,float fx0, float fx1, float fx2) {
         
-        return result;
+        float h = (x2 - x0) / 2;
+        
+        return (h/3) * ( fx0 + (4 * fx1) + fx2 );
+    }
+    
+    /**
+     * 
+     * @param x0 float The x0 given value
+     * @param x1 float The x1 given value
+     * @param x2 float The x2 given value
+     * @param x3 float The x3 given value
+     * @param fx0 float The f(x0) given value
+     * @param fx1 float The f(x1) given value
+     * @param fx2 float The f(x2) given value
+     * @param fx3 float The f(x3) given value
+     * @return result The result of Simpson's 1/3's rule applied
+     */
+    
+    public static float simpsonThreeEight(float x0, float x1, float x2, float x3, float fx0, float fx1, float fx2, float fx3) {
+        
+        float h = (x3 - x0) / 3;
+        
+        return ((3*h)/8) * ( fx0 + (3 * fx1) + (3 * fx2) + fx3 );
     }
     
     /**
